49c7a31c13d5cccc669796eb644f92b8fdaed039
Alexis Crespo <alexis-2cab@hotmail.com>
2021-01-15 19:43:57 -0500

    First test

---
+++ utilities.js
@@ -0,0 +1,9 @@
+function gathernNamesOf(arrayOfPeople){
+  return arrayOfPeople.map(person => person.age)
+}
+
+const people = [{name: 'Volt', name: 'Just', name: 'Hurts'}];
+
+const = names = gathernNamesOf(people);
+
+export {gathernNamesOf}
