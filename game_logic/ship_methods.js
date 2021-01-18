4bf25811905dbeba3ecabf1b68cafcd51f3d2d35
Alexis Crespo <alexis-2cab@hotmail.com>
2021-01-18 10:48:35 -0500

    method completed

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -1,5 +1,15 @@
-function checkForShip(){
+function checkForShip(player, coordinates){
+  var shipPresent, ship;
 
+  for(var i=0; i < player.ships.length; i++){
+    ship = player.ships[i];
+    shipPresent = ship.locations.filter(actualCoordinate => (actualCoordinate[0] === coordinates[0] && actualCoordinate[1] === coordinates[1]))[0];
+    if (shipPresent){
+      return true;
+    }
+  }
+  return false;
 }
+
 module.exports.checkForShip = checkForShip
 //
--- test/ship_test.js
+++ test/ship_test.js
@@ -3,7 +3,67 @@ var expect = require('chai').expect;
 describe('checkForShip', function(){
   var checkForShip = require('../game_logic/ship_methods').checkForShip;
 
-  it('should correctly report no ship at a given players coordinate', function(){
+  it('should correctly report a ship at given coordinate', function(){
 
+    var player = {
+      ships: [
+        {
+          locations: [[0, 0]]
+        }
+      ]
+    };
+
+    expect(checkForShip(player, [9, 9])).to.be.false;
   });
+
+  it('should correctly report a ship located at the given coordinates', function(){
+
+    var player = {
+      ships: [
+        {
+          locations: [[0, 0]]
+        }
+      ]
+    };
+
+    expect(checkForShip(player, [0, 0])).to.be.true;
+  });
+
+  it('should handle ship at more than one coordinate', function(){
+
+    var player = {
+      ships: [
+        {
+          locations: [[0, 0], [0, 1]]
+        }
+      ]
+    };
+    expect(checkForShip(player, [0, 1])).to.be.true;
+    expect(checkForShip(player, [0, 0])).to.be.true;
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
+
+  it('should handle checking multiple ships', function(){
+
+    var player = {
+      ships: [
+        {
+          locations: [[0, 0], [0, 1]]
+        },
+        {
+          locations: [[1, 0], [1, 1]]
+        },
+        {
+          locations: [[2, 0], [2, 1], [2, 2], [2, 3]]
+        }
+      ]
+    };
+    expect(checkForShip(player, [0, 1])).to.be.true;
+    expect(checkForShip(player, [0, 0])).to.be.true;
+    expect(checkForShip(player, [1, 0])).to.be.true;
+    expect(checkForShip(player, [1, 1])).to.be.true;
+    expect(checkForShip(player, [2, 3])).to.be.true;
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
+
 });
