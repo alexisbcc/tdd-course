68f03ec4f31e39e075cb4bb7542c4b37a09c4396
Alexis Crespo <alexis-2cab@hotmail.com>
2021-01-17 20:58:18 -0500

    Test with mocha

---
+++ test/main_test.js
@@ -0,0 +1,23 @@
+// var expect = require('chai').expect;
+//
+// function titleCase (title){
+//   var words = title.split(' ')
+//   var tittleCaseWords = words.map(word => word[0].toUpperCase() + word.substring(1));
+//   return tittleCaseWords.join(' ');
+// }
+//
+// expect(titleCase('the great mouse detective')).to.be.a('string');
+// expect(titleCase('a')).to.equal('A');
+// expect(titleCase('vertical')).to.equal('Vertical');
+//
+// expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
+
+// var expect = require('chai').expect;
+//
+// // Test suite
+// describe('Sanity check', function(){
+//   // Test spec (unit test)
+//   it('should run our test using npm', function(){
+//     expect(true).to.be.ok;
+//   });
+// });
